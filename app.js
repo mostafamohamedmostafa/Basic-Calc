dbe9e34ae11ddd2754f4930edfc652b6e0ccffac
mostafamohamedmostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2020-08-07 06:03:18 +0200

    adding clock

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+function showTime() {
+    let date = new Date();
+    let hours = date.getHours();//0-23
+    let minutes = date.getMinutes();//0-59
+    let seconds = date.getSeconds();//0-59
+
+    let formatHours = convertFormat(hours)
+    hours = checkTime(hours);
+document.getElementById('clock').innerHTML=`${hours}: ${minutes} : ${seconds} ${formatHours}`
+
+
+}
+
+function convertFormat(time) {
+    let format = 'AM'
+    if (time >= 12) {
+        format = 'PM'
+    }
+    return format;
+}
+
+function checkTime(time) {
+    if (time > 12) {
+        time = time - 12;
+    }
+
+    if (time ===0 ){
+        time = 12;
+    }
+    return time; 
+}
+
+showTime();
+setInterval(showTime,1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const btns = document.querySelectorAll('.btn');
 const screen = document.querySelector('.screen');
 const equalBtn = document.querySelector('.btn-equal');
@@ -17,9 +70,9 @@ equalBtn.addEventListener('click', function () {
     else {
         let valuee = eval(screen.value);
         screen.value = valuee;
-        }
-
     }
+
+}
 
 )
 
